88ea21b1f9173aa5e215804a1f94a86078ca5b1f
Egg Milk Soup <egg@milksoup.net>
2022-06-16 14:04:52 -0500

    remove village stuff from cow soup

--- src/main/java/net/fabricmc/eggmilksoup/EggMilkSoup.java
+++ src/main/java/net/fabricmc/eggmilksoup/EggMilkSoup.java
@@ -614,7 +614,7 @@ public class EggMilkSoup implements ModInitializer {
 
   public static final MobSoup COW_SOUP = new MobSoup (
     new FabricItemSettings().group(ItemGroup.FOOD).food(FoodComponents.SUSPICIOUS_STEW).maxCount(1),
-    new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32}
+    new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
   );
 
   @Override
