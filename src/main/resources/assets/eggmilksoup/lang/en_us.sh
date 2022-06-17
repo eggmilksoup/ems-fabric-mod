462dfe868b4f97a57a8fb00b8665f832677d252e
Egg Milk Soup <egg@milksoup.net>
2022-06-17 06:31:52 -0500

    fix two-word names for mobs

--- src/main/resources/assets/eggmilksoup/lang/en_us.sh
+++ src/main/resources/assets/eggmilksoup/lang/en_us.sh
@@ -1,9 +1,20 @@
 echo { > en_us.json
 
+separate()
+{
+  str="$(printf "$1" | cut -c 1 | tr [:lower:] [:upper:] | tr -d '\n'; printf "$1" | cut -c 2-)"
+  while echo $str | grep -q _
+  do
+    str="$(printf "$str" | cut -f 1 -d _ | tr -d '\n'; printf " "; printf "$str" | cut -f 2- -d _ | cut -c 1 | tr [:lower:] [:upper:] |
+      tr -d '\n'; printf "$str" | cut -f 2- -d _ | cut -c 2-)"
+  done
+  printf "$str"
+}
+
 for mob in glow_squid fox donkey cod chicken cat horse bat axolotl salmon pufferfish rabbit polar_bear pig piglin ocelot mule parrot wandering_trader sheep skeleton_horse villager snow_golem squid strider tropical_fish turtle goat enderman llama cave_spider bee panda zombified_piglin spider wolf dolphin trader_llama zoglin wither_skeleton witch blaze drowned creeper endermite evoker ghast guardian hoglin husk phantom piglin_brute magma_cube ravager shulker silverfish skeleton vindicator vex stray
 do
-  echo "  \"item.minecraft.${mob}_soup\": \"$(printf "$mob" | cut -c 1 | tr [:lower:] [:upper:] | tr -d '\n'; printf "$mob" | cut -c 2-) Soup\"," >> en_us.json
-  echo "  \"item.minecraft.${mob}_milk\": \"$(printf "$mob" | cut -c 1 | tr [:lower:] [:upper:] | tr -d '\n'; printf "$mob" | cut -c 2-) Milk\"," >> en_us.json
+  echo "  \"item.minecraft.${mob}_soup\": \"$(separate "${mob}_soup")\"," >> en_us.json
+  echo "  \"item.minecraft.${mob}_milk\": \"$(separate "${mob}_milk")\"," >> en_us.json
 done
 
 cat >> en_us.json <<EOF
